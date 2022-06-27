1e0d00dd0b9de919fda221a96c8be924c2441079
Adrian Gioia <afgioia@gmail.com>
2022-06-26 23:49:18 -0700

    Initial planning mockups and plan for project 4

---
+++ planning/tone-test/src/Blip.jsx
@@ -0,0 +1,16 @@
+const Blip = () => {
+    
+    // let color = []
+    // if (props.state[1] === 0) {
+        
+    // }
+    // console.log(props)
+
+    return(
+        <div className='blip'>
+            <p style={null}>I'm a blip!!!</p>
+        </div>
+    )
+}
+
+export default Blip
