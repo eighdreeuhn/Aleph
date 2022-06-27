f92e4f3785d28c0cbc9f0519a6d70d6c9a20a40c
Adrian Gioia <afgioia@gmail.com>
2022-06-27 10:38:20 -0700

    Imported aleph SVG and laid out file structure. Applied spin @keyframe, rendered it to the page.

--- aleph/src/App.js
+++ aleph/src/App.js
@@ -1,8 +1,14 @@
+import { useState, useEffect} from 'react'
+import Aleph from './Components/Aleph';
 import './App.css';
 
 function App() {
+
+  const [unsearch, setUnsearch] = useState('')
+
   return (
     <div className="App">
+      <Aleph />
     </div>
   );
 }
