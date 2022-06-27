5d9df25f43b4c96f6bf811b9c2df7d1d287a4226
Adrian Gioia <afgioia@gmail.com>
2022-06-27 12:24:52 -0700

    Opened the color hashing function Need to work out the mapping algorithm to return a unique RGB color ofr a given wavelength in the visible spectrum

--- aleph/src/App.js
+++ aleph/src/App.js
@@ -12,7 +12,7 @@ function App () {
   //----------Global constants--------//
 
   const A = 2 ** (1 / 12)
-  const ROOT = 110
+  const ROOT = 55
   let load = false
   let bpm
 
@@ -23,26 +23,35 @@ function App () {
 
   //----------main functions----------//
 
+  //Hashing function for transforming character codes into wavelengths in hertz//
+  //Converts each letter into its character code modulus 24 (two octaves of half-tones) and applies
+  //the function: f(n) = f(0) * A^(n) (hz)
   const interpolateNotes = phrase => {
-    return phrase.split('').map(l => ROOT * A ** (l.charCodeAt(0) % 24))
+    return phrase.split('').map(l => ROOT * A ** (l.charCodeAt(0) % 36))
   }
-  
+
+  const extractColors = phrase => {
+    return phrase.split('').map(l => l.charCodeAt(0))
+  }
+  //Search field change handler//
   const handlePhraseChange = function (e) {
     setUnsearch(e.target.value)
   }
-  
+  //Search for submit handler//
   const handlePhraseSubmit = function (e) {
     e.preventDefault()
     const notes = interpolateNotes(unsearch)
-    console.log(notes[0])
+    const colors = extractColors(unsearch)
+    console.log(colors)
     let unanswerCopy = {...unanswer}
     unanswerCopy.notes = notes
+    unanswerCopy.colors = colors
     setUnanswer(unanswerCopy)
     setUnsearch('')
     // play(notes)
   }
   //----------App rendering----------//
-  console.log(`And the answer is : ${unanswer.notes}`)
+  console.log(`  Color ranges:\n\n${unanswer.colors}\n\n  Notes matrix:\n\n${unanswer.notes}`)
   return (
     <div className='App'>
       <Aleph />
