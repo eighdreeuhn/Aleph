97e1fb87482ffa5d15cba4a435a26aaa4d665451
Adrian Gioia <afgioia@gmail.com>
2022-07-03 11:25:18 -0700

    Added the wind-catcher to the animation Will still need to adjust the swing for different screen sizes

--- aleph/src/Components/Windchime.jsx
+++ aleph/src/Components/Windchime.jsx
@@ -2,6 +2,7 @@ const Windchime = props => {
     
   return (
     <div className="windchimes-container">
+      <div className="top-bar"></div>
       <div className="center-rope"></div>
       <div className="chime chime-1"></div>
       <div className="chime chime-2"></div>
@@ -9,7 +10,7 @@ const Windchime = props => {
       <div className="chime chime-4"></div>
       <div className="chime chime-5"></div>
       <div className="chime chime-6"></div>
-      <div className="top-bar"></div>
+      <div className="wind-anchor"></div>
     </div>
   );
 };
